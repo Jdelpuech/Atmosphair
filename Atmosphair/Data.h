006be58475b1c7e0c5ac56104c0182112f08398d
Jdelpuech <juju.delpuech@gmail.com>
2019-05-20 15:59:16 +0200

    deplacement a la racine

--- Atmosphair/Data.h
+++ Atmosphair/Data.h
@@ -1,41 +1,48 @@
 /**************************************************************************************
- FileManager  -  Interface
+ Data  -  Interface
  -------------------
  début                : 10/05/2019
  copyright            : (C)2019 par Atmosph'Air
  ***************************************************************************************/
 
- //- Interface de la classe <FileManager> (fichier FileManager.h) ---------
-#if !defined ( FileManager_H )
-#define FileManager_H
+ //---------- Interface de la classe <Catalogue> (fichier Data.h) ----------------
+#if !defined ( Data_H )
+#define Data_H
 //---------------------------------------------------------------- Interfaces utilisées
 
 #include <string>
 #include <ctime>
 
+#include "Sensor.h"
 //-------------------------------------------------------------------------------------
-// Rôle de la classe <FileManager>
-/*
+// Rôle de la classe <Data>
+/*Les fichiers .csv définissant les mesures et les types de mesures seront modélisés 
+respectivement par les classes Data et DataType avec les mêmes attributs que ceux 
+stipulés dans les fichiers. 
  */
+
  //-------------------------------------------------------------------------------------
-class FileManager
+class Data
 {
 	//------------------------------------------------------------------------------ PUBLIC
 
 public:
-	//------------------------------------------------------------------ Méthodes publiques
+	//------------------------------------------------------------- Méthodes publiques
 
 
 	//---------------------------------------------------- Constructeurs - destructeur
-	FileManager(){}
+	Data(int DataImesureIdD,time_t timeStamp,double value,Sensor sensor);
+	// Mode d'emploi : à partir des données fournies par une ligne d'un fichier .csv
+	//décrivant une donnée, le constructeur permet de modéliser ce dernier à travers
+	//la construction d'un objet Data.
+	// Contrat : le fichier .csv est aux normes.
 
-	virtual ~FileManager(){}
+
+	virtual ~Data();
 	// Mode d'emploi : aucun
-    // Contrat : aucun
+	// Contrat : aucun
 
-
-
-	//------------------------------------------------------------------------------ PRIVE
+//------------------------------------------------------------------------------ PRIVE
 private:
 
 
@@ -44,11 +51,14 @@ protected:
 	//------------------------------------------------------------ Méthodes protégées
 
 	//------------------------------------------------------------ Attributs protégés
-
+	int DataID;
+	double lon;
+	double lat;
+	string description;
+	bool dysfonctionning;
 };
 
 
 
-#endif // FileManager_H
-
+#endif // Data_H
 
