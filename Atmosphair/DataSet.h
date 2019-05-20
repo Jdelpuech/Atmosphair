9b4b91cf17655f2cb84066f5ca0f6aa84d572974
BuildTools <hugo@null.spigotmc.org>
2019-05-20 16:02:34 +0200

    ajout data set

---
+++ Atmosphair/DataSet.h
@@ -0,0 +1,43 @@
+/**************************************************************************************
+ DataSet  -  Interface
+ -------------------
+ début                : 10/05/2019
+ copyright            : (C)2019 par Atmosph'Air
+ ***************************************************************************************/
+
+ //- Interface de la classe <DataSet> (fichier DataSet.h) ---------
+#if !defined ( DataSet_H )
+#define DataSet_H
+//---------------------------------------------------------------- Interfaces utilisées
+#include "DataSet.h"
+#include "Sensor.h"
+#include "User.h"
+//-------------------------------------------------------------------------------------
+// Rôle de la classe <DataSet>
+/*Application Manager est le noyau de notre application. En tant que dispatcher, il gèrera
+ l’appel aux méthodes de chaque classe afin de répondre aux attentes de l'utilisateur.
+ */
+ //-------------------------------------------------------------------------------------
+class DataSet
+{
+	//------------------------------------------------------------------------------ PUBLIC
+
+public:
+	//------------------------------------------------------------------ Méthodes publiques
+
+
+
+
+	//------------------------------------------------------------------------------ PRIVE
+private:
+
+
+	//-----------------------------------------------------------------------------PROTEGE
+protected:
+	//------------------------------------------------------------ Méthodes protégées
+
+	//------------------------------------------------------------ Attributs protégés
+
+};
+
+#endif // DataSet_H
